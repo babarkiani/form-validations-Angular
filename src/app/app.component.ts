474ed1ed40308508c981e3f1e6ff0c8303fdc7c2
Babar kk <mabkiani36@gmail.com>
2019-01-22 11:37:47 +0500

    my first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-validation';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  get f() {return this.form.controls;}
+
+  ngOnInit(){
+  this.form =this.fb.group({
+    firstname: ['', Validators.minLength(3)],
+    // new FormControl("", Validators.compose([
+    //   Validators.required, 
+    //  Validators.maxLength(2),
+    //   Validators.pattern('[\\w\\-\\s\\/+]')
+    // ]) ),
+    lastname: new FormControl(""),
+    languages: new FormControl("")
+
+  });
+}
+
+textValidator(control){
+  if(control.value.length < 3){
+    return {'lastname': true};
+  }
+}
+
+
+onSubmit = function(user){
+  console.log(user);
+}
 }
